014fabe33d7416196490e7a9758aba2d5b0c4252
Kamekichi1357 <kamekichi1012@gmail.com>
2017-01-23 14:38:28 +0900

    ジャヴァスクリプトの状態
    C
    D

---
+++ jQuerySample/javascript/alert.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+
+	$('#button').click(function() {
+	alert("クリックされました");
+	});
+
+   /*var buttom = document.getElementById('button');
+
+   button.addEventListener('click', function(){
+     alert("クリックされました");
+   });*/
+}
